0aad919b44d0c21be0e2449b469dc2d3c3720460
stephenbrittain <brittain@udel.edu>
2022-07-28 12:34:22 -0400

    Split up into different files and added more functions.

--- main.py
+++ main.py
@@ -3,48 +3,25 @@
 
 # todo: nest code in desired nets folder OR import nets to repository
 
-import json  # Allows us to read json files
-import numpy as np
-from matplotlib import pyplot as plt  # Imports matplotlib so we can plot coordinates
+from functions import *
 
 # creates name of the file that needs to be open
 name = input("Enter the type of polyhedra:")
 number = input("Enter the net number:")
 filename = name + str(number) + '.json'
 
-with open(filename) as json_file:  # Calls a particular .json file
-    data = json.load(json_file)  # Stores the contents of the database as a list
-    v = np.array(data.get("Vertices"))  # Calls database entry as a list and then converts to an array
-    e = np.array(data.get("Edges"))  # Calls database entry as a list and then converts to an array
-    # x = data.keys() #Stores as a list the names of all the entries in the dictionary.
+data = loadfile(name, number)
+v = np.array(data.get("Vertices"))  # Calls database entry as a list and then converts to an array
+e = np.array(data.get("Edges"))  # Calls database entry as a list and then converts to an array
+x = data.keys()  # Stores as a list the names of all the entries in the dictionary.
+f = np.array(data.get("Faces"))
+facegraph = np.array(data["FaceGraph"]["AdjMat"].get("matrix"))
 
-
-''' 
-Graphnet is a function that takes a list of vertices and edges as an input
-and outputs a plot of the net
-
-:args
-vlist: array of vertices from data
-elist: array of edges from data
-
-'''
-def graphnet(vlist, elist):
-    w, z = vlist.T  # not really sure what this does
-    plt.scatter(w, z)  # plots the vertices
-
-    # For each edge in edge list,
-    # Finds the coordinates for each endpoint and plots the line segment
-    for x in elist:
-        # print(x)
-        point1 = vlist[x[0]]
-        point2 = vlist[x[1]]
-        # print(point1)
-        # print(point2)
-        x_values = [point1[0], point2[0]]
-        y_values = [point1[1], point2[1]]
-        plt.plot(x_values, y_values, 'bo', linestyle="-")
-    plt.axis('scaled')  # Preserves 1:1 aspect ratio
-    plt.show()  # Plots the scatterplot
-
-
-graphnet(v, e)
+print('Radius of Gyration = ' + str(radiusg(v, f)))
+graphnet(v, e, 'blue')  # Plots the net
+print('Number of Vertex Connections = ' + str(countvc(name, v, e)))
+graphnet(findcenters(v, f), facegraph, 'red')  # plots spanning tree of the net
+# print(facegraph)
+# print(f)
+plt.axis('scaled')  # Preserves 1:1 aspect ratio
+plt.show()  # Plots the scatterplot
