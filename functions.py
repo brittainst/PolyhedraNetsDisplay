08f439dcbcefa61943b0c7b0e6cdc980fce7bd0e
stephenbrittain <brittain@udel.edu>
2022-08-03 18:55:22 -0400

    Wrote code to draw Schlegel diagrams as well as Durer Nets

--- functions.py
+++ functions.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt  # Imports matplotlib so we can plot coordi
 import json  # Allows us to read json files
 
 
+
 def loadfile(filename):
     with open(filename) as json_file:  # Calls a particular .json file
         data = json.load(json_file)  # Stores the contents of the database as a list
@@ -254,3 +255,219 @@ def diameter(name, number):
                         pathtracker[len(pathtracker)].append(int(edge[1]))
         longestpathsbyvertex.append(pathtracker)
     return longestpathsbyvertex
+
+'''
+function drawnet
+'''
+# todo: move drawnet to functions
+def drawnet(name, number):
+    filename = name + 'Net' + str(number) + '.json'
+
+    data = loadfile(filename)
+    v = np.array(data.get("Vertices"))  # Calls database entry as a list and then converts to an array
+    e = np.array(data.get("Edges"))  # Calls database entry as a list and then converts to an array
+    x = data.keys()  # Stores as a list the names of all the entries in the dictionary.
+    f = np.array(data.get("Faces"))
+    facegraph = np.array(data["FaceGraph"]["AdjMat"].get("matrix"))
+
+    # print('Radius of Gyration = ' + str(radiusg(v, f)))
+    graphnet(v, e, 'blue', False)  # Plots the net
+    vertconnect = str(countvc(name, v, e, True))
+    print('Number of Vertex Connections = ' + vertconnect)
+    # print('The leaves are ' + str(leaves(name,number)))
+
+    #UNCOMMENT THIS LINE TO PLOT SPANNING TREE OF THE NET
+    #graphnet(findcenters(v, f), facegraph, 'red', False)  # plots spanning tree of the net
+    centers = findcenters(v, f)
+    for i in range(0, len(centers)):
+        plt.text(centers[i][0], centers[i][1], str(i), fontsize=12, horizontalalignment='center',
+             verticalalignment='center')
+
+    # print(facegraph)
+    firstface = f[0]
+    xcoord = 0
+    ycoord = 0
+    for i in firstface:
+        xcoord = xcoord + v[int(firstface[i])][0]
+        ycoord = ycoord + v[int(firstface[i])][1]
+    xcoord = xcoord / len(firstface)
+    ycoord = ycoord / len(firstface)
+
+    # THESE TWO LINES PRINT NETID ON THE NET
+    #plt.text(xcoord, ycoord, str(number), fontsize=8, horizontalalignment='center',
+    #         verticalalignment='center')
+
+    plt.axis('scaled')  # Preserves 1:1 aspect ratio
+    plt.xlabel(name + ' Net ' + str(number) + ': V_c = ' + str(vertconnect))
+
+    FaceCenters = findcenters(v, f)
+    centermass = [0, 0]  # Initializes a variable for center of mass
+    for center in FaceCenters:  # Averages the centers of the faces to find center of mass
+        centermass = np.add(centermass, center)
+    centermass[0] = centermass[0] / len(FaceCenters)
+    centermass[1] = centermass[1] / len(FaceCenters)
+
+    plt.xlim([centermass[0] - 7, centermass[0] + 7])
+    plt.ylim([centermass[1] - 7, centermass[1] + 7])
+    plt.savefig("output" + str(number) + ".jpg", dpi=600)
+    #plt.show()  # Plots the scatterplot
+
+
+def neighbors(face, bindlist):
+    list_of_neighbors = []
+    for binding in bindlist:
+        if binding[0] == face:
+            list_of_neighbors.append(binding[1])
+        if binding[1] == face:
+            list_of_neighbors.append(binding[0])
+    return(list_of_neighbors)
+'''
+draw_schlegel is a function that draws the schlegel diagram of a dodecahedron
+'''
+def draw_schlegel(name,number):
+    data = loadfile("dodecahedron.json")
+    x = data.keys()  # Stores as a list the names of all the entries in the dictionary.
+    y = data.get("links")
+    z = data.get("nodes")
+    e = []
+    for i in range(30):
+        e.append([y[i]["source"], y[i]["target"]])
+
+    v = []
+    for i in range(20):
+        v.append([z[i]["x"], z[i]["y"]])
+
+    graphnet(np.array(v), np.array(e), "blue", True)
+
+    internal_faces = [[0, 1, 2, 3, 19],
+                      [1, 2, 6, 7, 8],
+                      [2, 3, 4, 5, 6],
+                      [3, 4, 17, 18, 19],
+                      [0, 10, 11, 18, 19],
+                      [0, 1, 8, 9, 10],
+                      [5, 6, 7, 14, 15],
+                      [7, 8, 9, 13, 14],
+                      [9, 10, 11, 12, 13],
+                      [11, 12, 16, 17, 18],
+                      [4, 5, 15, 16, 17]]
+    centers_of_faces = []
+    for face in internal_faces:
+        xsum = 0
+        ysum = 0
+        for vertex in face:
+            xsum += v[vertex][0]
+            ysum += v[vertex][1]
+        centers_of_faces.append([xsum/5, ysum/5])
+    centers_of_faces.append([4, 4])
+
+    filename = name + 'Net' + str(number) + '.json'
+
+    data2 = loadfile(filename)
+    g = np.array(data2.get("DihedralAngles"))
+    face_bindings = []
+    for i in range(30):
+        face_bindings.append([g[i]["Face-0"], g[i]["Face-1"]])
+
+    #print(face_bindings)
+
+    face_ordering = [0]
+    # finds face 1
+    for i in range(12):
+        found = 0
+        if i not in face_ordering:
+            for binding in face_bindings:
+                if binding[0] == 0:
+                    face_ordering.append(binding[1])
+                    found += 1
+                if binding[1] == 0:
+                    face_ordering.append(binding[0])
+                    found += 1
+                if found == 1:
+                    break
+        if found ==1:
+            break
+    # finds face 2
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[0] in list_of_neighbors and face_ordering[1] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 3
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[0] in list_of_neighbors and face_ordering[2] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 4
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[0] in list_of_neighbors and face_ordering[3] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 5
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[0] in list_of_neighbors and face_ordering[4] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 6
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[1] in list_of_neighbors and face_ordering[2] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 7
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[1] in list_of_neighbors and face_ordering[5] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 8
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[4] in list_of_neighbors and face_ordering[5] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 9
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[3] in list_of_neighbors and face_ordering[4] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 10
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[2] in list_of_neighbors and face_ordering[3] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 11
+    for i in range(12):
+        if i not in face_ordering:
+            list_of_neighbors = neighbors(i, face_bindings)
+            if face_ordering[0] in list_of_neighbors and face_ordering[2] in list_of_neighbors:
+                face_ordering.append(i)
+                break
+    # finds face 3
+    for i in range(12):
+        if i not in face_ordering:
+            face_ordering.append(i)
+
+    #print(face_ordering)
+    for i in range(12):
+        plt.text(centers_of_faces[i][0], centers_of_faces[i][1], str(face_ordering[i]), fontsize=12, horizontalalignment='center',
+             verticalalignment='center')
+    #for i in range(20):
+    #    plt.text(v[i][0], v[i][1], str(i), fontsize=12)
+
+    plt.axis('scaled')
+    #f = np.array(data.get("Faces"))
+    #facegraph = np.array(data["FaceGraph"]["AdjMat"].get("matrix"))
--- main.py
+++ main.py
@@ -2,55 +2,13 @@
 # number of that net (according to the database) to open. it will then draw the net
 
 from functions import *
-
+#from make_dodecahedron_graph import *
 # creates name of the file that needs to be open
 name = input("Enter the type of polyhedra:")
 
 
-# number = input("Enter the net number:")
+number = input("Enter the net number:")
 
-# todo: move drawnet to functions
-def drawnet(name, number):
-    filename = name + 'Net' + str(number) + '.json'
-
-    data = loadfile(filename)
-    v = np.array(data.get("Vertices"))  # Calls database entry as a list and then converts to an array
-    e = np.array(data.get("Edges"))  # Calls database entry as a list and then converts to an array
-    x = data.keys()  # Stores as a list the names of all the entries in the dictionary.
-    f = np.array(data.get("Faces"))
-    facegraph = np.array(data["FaceGraph"]["AdjMat"].get("matrix"))
-
-    # print('Radius of Gyration = ' + str(radiusg(v, f)))
-    graphnet(v, e, 'blue', False)  # Plots the net
-    vertconnect = str(countvc(name, v, e, True))
-    print('Number of Vertex Connections = ' + vertconnect)
-    # print('The leaves are ' + str(leaves(name,number)))
-    graphnet(findcenters(v, f), facegraph, 'red', False)  # plots spanning tree of the net
-    # print(facegraph)
-    firstface = f[0]
-    xcoord = 0
-    ycoord = 0
-    for i in firstface:
-        xcoord = xcoord + v[int(firstface[i])][0]
-        ycoord = ycoord + v[int(firstface[i])][1]
-    xcoord = xcoord / len(firstface)
-    ycoord = ycoord / len(firstface)
-    plt.text(xcoord, ycoord, str(number), fontsize=8, horizontalalignment='center',
-             verticalalignment='center')
-    plt.axis('scaled')  # Preserves 1:1 aspect ratio
-    plt.xlabel(name + ' Net ' + number + ': V_c = ' + vertconnect)
-
-    FaceCenters = findcenters(v, f)
-    centermass = [0, 0]  # Initializes a variable for center of mass
-    for center in FaceCenters:  # Averages the centers of the faces to find center of mass
-        centermass = np.add(centermass, center)
-    centermass[0] = centermass[0] / len(FaceCenters)
-    centermass[1] = centermass[1] / len(FaceCenters)
-
-    plt.xlim([centermass[0] - 7, centermass[0] + 7])
-    plt.ylim([centermass[1] - 7, centermass[1] + 7])
-    plt.savefig("output" + str(number) + ".jpg", dpi=600)
-    plt.show()  # Plots the scatterplot
 
 
 array = [35048, 43253, 43374, 19031, 27884, 22840, 41635, 16750, 10337, 33987, 9025, 23207, 33980, 40881, 32036, 5886,
@@ -58,6 +16,11 @@ array = [35048, 43253, 43374, 19031, 27884, 22840, 41635, 16750, 10337, 33987, 9
 array2 = [7725, 9023, 11463, 13645, 18975, 22159, 24417, 33681, 35048, 35934, 39346, 40685, 41417, 41971, 42758, 43227,
           43253, 43374]
 
-for i in array2:
-    number = str(i).zfill(5)
-    drawnet(name, number)
+#for i in array2:
+#    number = str(i).zfill(5)
+#    drawnet(name, number)
+plot1 = plt.figure(1)
+drawnet(name, number)
+plot2 = plt.figure(2)
+draw_schlegel(name, number)
+plt.show()
