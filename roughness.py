6d3ccebabb909c16175dd42eed47928bfe54bea2
faithlovell <faithlxvell@gmail.com>
2022-09-12 13:37:58 -0400

    refactored all variables & function names for roughness.py

--- roughness.py
+++ roughness.py
@@ -5,21 +5,21 @@ from functions import *
 
 name = "Icosahedron"
 
-minrough = 1000
-maxrough = 0
+minRough = 1000
+maxRough = 0
 possible_values = []
 for i in range(43380):
     # calls the appropriate file from the database and stores it as the dictionary data
     #TODO: loadfile thing
     data = loadFile(name, str(i).zfill(5))
     roughness = data["NetInfo"].get("Roughness")
-    if roughness > maxrough:
-        maxrough = roughness
-    if roughness < minrough:
-        minrough = roughness
+    if roughness > maxRough:
+        maxRough = roughness
+    if roughness < minRough:
+        minRough = roughness
     if roughness not in possible_values:
         possible_values.append(roughness)
-print("maximum roughness = " + str(maxrough))
-print("minimum roughness = " + str(minrough))
+print("maximum roughness = " + str(maxRough))
+print("minimum roughness = " + str(minRough))
 print("possible values" + str(possible_values))
 print(len(possible_values))
